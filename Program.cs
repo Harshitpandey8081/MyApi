cd92c61790bfc7ae4f37a282e1e99ce0ad2bc34f
Harshit Pandey <panditharshit1@gmail.com>
2025-11-14 19:46:18 +0530

    test

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/test", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
