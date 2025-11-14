f766a8a57cdc1c27b30a4be2839efe6fdc0214b4
Harshit Pandey <panditharshit1@gmail.com>
2025-11-14 20:58:55 +0530

    test

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    "test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"
 };
 
 app.MapGet("/test", () =>
